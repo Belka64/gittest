778baa51445c8c03873f0ddfa2e1e0014e645999
blk <vby@nemetos.com>
2015-01-08 16:52:26 +0200

    addede files

---
+++ LogFilesFetcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Nemetos.BCH.Importers.UrlInfo.Domain;
+
+namespace Nemetos.BCH.Importers.UrlInfo
+{
+    public class LogFilesFetcher
+    {
+        public List<IisLogFile> IisLogFiles { get; private set; }
+
+        public LogFilesFetcher(string logPath)
+        {
+            this.IisLogFiles = new List<IisLogFile>();
+            List<string> filePaths = Directory.GetFiles(logPath, "*.log").ToList();
+            foreach (var filePath in filePaths)
+            {
+                this.IisLogFiles.Add(new IisLogFile(filePath));
+            }
+        }
+
+        public IEnumerable<IisLogFile> GetIisLogFiles(DateTime startDate, DateTime endDate) { return this.IisLogFiles.Where(x => x.LogDate >= startDate && x.LogDate <= endDate); }
+    }
+}
---
+++ UrlInfo.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI;
+using Nemetos.BCH.Importers.UrlInfo.Domain;
+
+namespace Nemetos.BCH.Importers.UrlInfo
+{
+    public partial class UrlInfo : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void GetInfoAboutUrlButtonClick(object sender, EventArgs e)
+        {
+            var logFilesFetcher = new LogFilesFetcher(this.pathTextBox.Text);
+            LogParserWrapper logParserWrapper = new LogParserWrapper();
+            IEnumerable<IisLogFile> iisLogs = logFilesFetcher.GetIisLogFiles(this.startDateCalendar.SelectedDate, this.endDateCalendar.SelectedDate);
+            var countCollection = logParserWrapper.GetUrlCount(this.urlTextbox.Text, iisLogs, true);
+            this.Response.Write(countCollection.Sum(x => Convert.ToInt32(x.Count)));
+
+
+            
+            ResultGrid.DataSource = countCollection;
+            ResultGrid.DataBind();
+        }
+    }
+}
